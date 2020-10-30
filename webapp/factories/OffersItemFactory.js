6c657c35651905392edee82dc5ef398fb0846414
Matteo Prinetti <matteo.prinetti@mgb.ch>
2020-10-30 14:12:06 +0000

    Added week status icon (tentatively has no data on DA1)

--- webapp/factories/OffersItemFactory.js
+++ webapp/factories/OffersItemFactory.js
@@ -1,8 +1,23 @@
 sap.ui.define([], function () {
 	"use strict";
+
 	return {
 
 		factory: function (sId, oContext) {
+
+			function getWeekNumber(d) {
+				// Copy date so don't modify original
+				d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+				// Set to nearest Thursday: current date + 4 - current day number
+				// Make Sunday's day number 7
+				d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+				// Get first day of year
+				var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+				// Calculate full weeks to nearest Thursday
+				var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+				// Return array of year and week number
+				return [d.getUTCFullYear(), weekNo];
+			};
 
 			var itemFactory = new sap.m.ColumnListItem({
 				type: "Inactive"
@@ -12,20 +27,38 @@ sap.ui.define([], function () {
 				title: oContext.getObject().ZzExtOfrId,
 				text: oContext.getObject().OfrName
 			}));
-		
+
 			// Status is now an Icon.
 
-/*			itemFactory.addCell(new sap.m.Label({
-				text: oContext.getObject().OfrSubStateCd,
-				tooltip: oContext.getObject().OfrSubStateDescr
-			}));*/
+			/*			itemFactory.addCell(new sap.m.Label({
+							text: oContext.getObject().OfrSubStateCd,
+							tooltip: oContext.getObject().OfrSubStateDescr
+						}));*/
+
+			var actualweek = getWeekNumber(new Date(this.getView().byId("calenderAuswahl").getDateValue()));
+			var startweek = getWeekNumber(new Date(oContext.getObject().Startdat));
+			var endweek = getWeekNumber(new Date(oContext.getObject().Enddat));
+
+			// different cases: 
+			// neu (Angebot startet in der ausgewählten Woche)
+			// auslaufend (Enddatum liegt in der selektierten Woche)
+			// läuft (Angebot lief bereits in der Vorwoche)
+			// abgelaufen	SE Angebote (Sellout) dürfen nach dem Ablaufdatum auf der Fläche bleiben
+
+			var iconsrc="";
+			
+			if(actualweek[1]===startweek[1]) iconsrc="sap-icon://begin"; // start now
+			if(actualweek[1] > startweek[1]) iconsrc="sap-icon://restart"; // start already
+			if(actualweek[1] === endweek[1]) iconsrc="sap-icon://end"; // start already
 
 			itemFactory.addCell(new sap.ui.core.Icon({
-				src: "sap-icon://restart"
+				src: iconsrc
 			}).addStyleClass("zPolyLargeIcon"));
 
-
-			var campaign = { CampaignId: "", CampaignName: ""};
+			var campaign = {
+				CampaignId: "",
+				CampaignName: ""
+			};
 			var campaigns = this.getModel("Offers").getProperty(oContext.getPath() + "/toCampaign");
 
 			// get the first campaign in case it has more - not sure if this is ok
@@ -39,20 +72,20 @@ sap.ui.define([], function () {
 			}));
 
 			// Planungsart 
-			
+
 			itemFactory.addCell(new sap.m.Label({
 				text: oContext.getObject().ZzPlaartTxt
 			}));
-			
+
 			// Angebotsart 
-			
+
 			itemFactory.addCell(new sap.m.ObjectIdentifier({
 				title: oContext.getObject().PromoType,
-				text: oContext.getObject().PromoTypeTxt.substring(0,10)
+				text: oContext.getObject().PromoTypeTxt.substring(0, 10)
 			}));
-			
+
 			// Kommunikationsrolle
-			
+
 			itemFactory.addCell(new sap.m.Label({
 				text: "Defcon 5"
 			}));
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,5 +1,20 @@
 sap.ui.define([], function () {
 	"use strict";
+	
+	function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return [d.getUTCFullYear(), weekNo];
+   }
+
 	return {
 		isPromoFlaeche: function (sStatus) {
 			if (sStatus === "1") return !true;
@@ -20,6 +35,20 @@ sap.ui.define([], function () {
 			// only the last 12 char are the product 
 			return "/sap/opu/odata/sap/ZR_MEDIAEXPORT_SRV/HauptBildSet(Artnr='" + oValue.substr(oValue.length - 12) +
 				"',Format='web240x310')/$value";
-		} 
+		},
+		
+		// Status als Icon 
+		// neu (Angebot startet in der ausgewählten Woche)
+		// auslaufend (Enddatum liegt in der selektierten Woche)
+		// läuft (Angebot lief bereits in der Vorwoche)
+		// abgelaufen
+		// SE Angebote (Sellout) dürfen nach dem Ablaufdatum auf der Fläche bleiben
+		
+		getOfferIcon: function(sStartDatum,sEndDatum) {
+			if( sStartDatum === undefined) return "";
+			
+		}
+		
+		
 	};
 });
